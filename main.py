eed33855a18f4ec7c0db4b5070e990a1bb916c1b
Tate <tatehentrich@gmail.com>
2023-09-28 10:30:38 -0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+
+class KivyFileApp(App):
+    pass
+
+
+class LayoutExample(BoxLayout):
+    pass
+
+class BoxLayoutExample():
+    pass
+KivyFileApp().run()
